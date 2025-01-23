2a974350639a6b5cfe4c598cdc9ea97d33e714b9
Hussain Alwazan <230049123@aston.ac.uk>
2025-01-23 10:22:48 +0000

    Edited Lab1_Java in GitPod

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -5,6 +5,7 @@ void testMethod1()
   //First command x = y + z
   //Second commnad print(x)
   // Edit by Hussain_230049123
+  // Edit by Hussain_230049123 in GitPod
 
   //Last line print('Good bye')
 }
